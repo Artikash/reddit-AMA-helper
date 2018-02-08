47abc3f1fe915ae83b35cf36d35c7af7c1449fa9
Akash Mozumdar <akashmozumdar@gmail.com>
2018-02-08 11:06:17 -0500

    connect to reddit

--- node.js
+++ node.js
@@ -1,5 +1,13 @@
 const Snoowrap = require("snoowrap");
-const {
-  user, password, clientID, clientSecret,
-} = require("./secrets");
+const Snoostorm = require("snoostorm");
+const reddit = new Snoowrap(require("./secrets"));
 
+const client = new Snoostorm(reddit);
+const stream = client.SubmissionStream(/*{ subreddit: "IAmA" }*/);
+
+stream.on("submission", (post) => {
+  console.log(post);
+  if (post.author);
+  //post.
+  setTimeout(() => stream.emit("stop"), 10000);
+});
